2a3947d859fd59c4b4bfff93f00c48214fb67280
neil-morgan <52212097+neil-morgan@users.noreply.github.com>
2021-10-07 21:55:00 +0100

    fixing responsive

--- src/components/layout/carousel/Slider.js
+++ src/components/layout/carousel/Slider.js
@@ -1,5 +1,11 @@
 import { useEffect, useCallback } from "react";
-import { useMediaQuery, useTheme, Flex, Box } from "@chakra-ui/react";
+import {
+  useMediaQuery,
+  useBreakpointValue,
+  useTheme,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
 import { useBoundingRect } from "../../../hooks";
 import Navigator from "./Navigator";
 
@@ -20,17 +26,48 @@ const Slider = ({
 }) => {
   const [sliderRef, { width }] = useBoundingRect();
   const { breakpoints } = useTheme();
+  const bp = useBreakpointValue({
+    base: "base",
+    sm: "sm",
+    md: "md",
+    lg: "lg",
+    xl: "xl",
+    xxl: "xxl",
+  });
 
-  const [isBetweenBaseAndSm] = useMediaQuery(
-    `(min-width: ${breakpoints.base}) and (max-width: ${breakpoints.sm})`
-  );
-  const [isBetweenSmAndMd] = useMediaQuery(
-    `(min-width: ${breakpoints.sm}) and (max-width: ${breakpoints.md})`
-  );
-  const [isBetweenMdAndXl] = useMediaQuery(
-    `(min-width: ${breakpoints.md}) and (max-width: ${breakpoints.xl})`
-  );
-  const [isGreaterThanXL] = useMediaQuery(`(min-width: ${breakpoints.xl})`);
+  console.log(bp);
+  // const [
+  //   isBetweenBaseAndSm,
+  //   isBetweenSmAndMd,
+  //   isBetweenMdAndLg,
+  //   isBetweenLgAndXl,
+  //   isBetweenXlAndXxl,
+  //   isGreaterThanXxl,
+  // ] = useMediaQuery([
+  //   `(min-width: ${breakpoints.base}) and (max-width: ${breakpoints.sm})`,
+  //   `(min-width: ${breakpoints.sm}) and (max-width: ${breakpoints.md})`,
+  //   `(min-width: ${breakpoints.md}) and (max-width: ${breakpoints.lg})`,
+  //   `(min-width: ${breakpoints.lg}) and (max-width: ${breakpoints.xl})`,
+  //   `(min-width: ${breakpoints.xl}) and (max-width: ${breakpoints.xxl})`,
+  //   `(min-width: ${breakpoints.xxl})`,
+  // ]);
+
+  // const [isBetweenBaseAndSm] = useMediaQuery(
+  //   `(min-width: ${breakpoints.base}) and (max-width: ${breakpoints.sm})`
+  // );
+  // const [isBetweenSmAndMd] = useMediaQuery(
+  //   `(min-width: ${breakpoints.sm}) and (max-width: ${breakpoints.md})`
+  // );
+  // const [isBetweenMdAndLg] = useMediaQuery(
+  //   `(min-width: ${breakpoints.md}) and (max-width: ${breakpoints.lg})`
+  // );
+  // const [isBetweenLgAndXl] = useMediaQuery(
+  //   `(min-width: ${breakpoints.lg}) and (max-width: ${breakpoints.xl})`
+  // );
+  // const [isBetweenXlAndXxl] = useMediaQuery(
+  //   `(min-width: ${breakpoints.xl}) and (max-width: ${breakpoints.xxl})`
+  // );
+  // const [isGreaterThanXxl] = useMediaQuery(`(min-width: ${breakpoints.xxl})`);
 
   const handleFocus = () => setTrackIsActive(true);
 
@@ -55,7 +92,7 @@ const Slider = ({
     );
   };
 
-  const handleSm = useCallback(() => {
+  const handleBase = useCallback(() => {
     setItemWidth(Math.round(width) / 2);
     setIsDisabled(positions.length <= 2);
     setMultiplier(0.65);
@@ -69,7 +106,7 @@ const Slider = ({
     width,
   ]);
 
-  const handleMd = useCallback(() => {
+  const handleSm = useCallback(() => {
     setItemWidth(Math.round(width) / 3);
     setIsDisabled(positions.length <= 3);
     setMultiplier(0.55);
@@ -83,7 +120,7 @@ const Slider = ({
     width,
   ]);
 
-  const handleLg = useCallback(() => {
+  const handleMd = useCallback(() => {
     setItemWidth(Math.round(width) / 4);
     setIsDisabled(positions.length <= 4);
     setMultiplier(0.45);
@@ -97,7 +134,35 @@ const Slider = ({
     width,
   ]);
 
+  const handleLg = useCallback(() => {
+    setItemWidth(Math.round(width) / 5);
+    setIsDisabled(positions.length <= 5);
+    setMultiplier(0.45);
+    setConstraint(5);
+  }, [
+    setIsDisabled,
+    setConstraint,
+    setMultiplier,
+    setItemWidth,
+    positions,
+    width,
+  ]);
+
   const handleXl = useCallback(() => {
+    setItemWidth(Math.round(width) / 6);
+    setIsDisabled(positions.length <= 6);
+    setMultiplier(0.45);
+    setConstraint(6);
+  }, [
+    setIsDisabled,
+    setConstraint,
+    setMultiplier,
+    setItemWidth,
+    positions,
+    width,
+  ]);
+
+  const handleXxl = useCallback(() => {
     setItemWidth(
       positions.length <= 4
         ? Math.round(width) / 5
@@ -118,20 +183,49 @@ const Slider = ({
   ]);
 
   useEffect(() => {
-    isBetweenBaseAndSm && handleSm();
-    isBetweenSmAndMd && handleMd();
-    isBetweenMdAndXl && handleLg();
-    isGreaterThanXL && handleXl();
-  }, [
-    isBetweenBaseAndSm,
-    isBetweenSmAndMd,
-    isBetweenMdAndXl,
-    isGreaterThanXL,
-    handleSm,
-    handleMd,
-    handleLg,
-    handleXl,
-  ]);
+    switch (bp) {
+      case "base":
+        handleBase();
+        break;
+      case "sm":
+        handleSm();
+        break;
+      case "md":
+        handleMd();
+        break;
+      case "lg":
+        handleLg();
+        break;
+      case "xl":
+        handleXl();
+        break;
+      case "xxl":
+        handleXl();
+        break;
+    }
+  }, [bp, handleBase, handleSm, handleMd, handleLg, handleXl, handleXxl]);
+
+  // useEffect(() => {
+  //   isBetweenBaseAndSm && handleBase();
+  //   isBetweenSmAndMd && handleSm();
+  //   isBetweenMdAndLg && handleMd();
+  //   isBetweenLgAndXl && handleLg();
+  //   isBetweenXlAndXxl && handleXl();
+  //   isGreaterThanXxl && handleXxl();
+  // }, [
+  //   isBetweenBaseAndSm,
+  //   isBetweenSmAndMd,
+  //   isBetweenMdAndLg,
+  //   isBetweenLgAndXl,
+  //   isBetweenXlAndXxl,
+  //   isGreaterThanXxl,
+  //   handleBase,
+  //   handleSm,
+  //   handleMd,
+  //   handleLg,
+  //   handleXl,
+  //   handleXxl,
+  // ]);
 
   const navigatorProps = {
     handleDecrementClick,
